a331ddf24963c364b54f5175315c3840d7f91563
David Mansy <david.mansy@icloud.com>
2018-09-15 11:58:23 +0200

    feat: Implement a tab displaying geolocation info from the phone

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import { white, purple } from './utils/colors';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import { Constants } from 'expo';
 import EntryDetail from './components/EntryDetail';
+import Live from './components/Live';
 
 function UdaciStatusBar({ backgroundColor, ...props }) {
   return (
@@ -39,6 +40,15 @@ const Tabs = createBottomTabNavigator(
         tabBarLabel: 'Add Entry',
         tabBarIcon: ({ tintColor }) => (
           <FontAwesome name="plus-square" size={30} color={tintColor} />
+        )
+      }
+    },
+    Live: {
+      screen: Live,
+      navigationOptions: {
+        tabBarLabel: 'Live',
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-speedometer" size={30} color={tintColor} />
         )
       }
     }
